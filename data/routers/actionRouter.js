14f9a1374d396e313835f4dc9f350ec911818424
Pete1701 <pete.pertongonas@pirron.one>
2020-04-10 19:55:05 +0200

    002

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -5,4 +5,39 @@ const Action = require('../helpers/actionModel.js');
 
 const router = express.Router();
 
+router.get('/:id', (req, res) => {
+    Action.get(req.params.id)
+      .then((actions) => {
+        if (actions) {
+          res.status(200).json(actions);
+        } else {
+          res.status(404).json({ message: "The action with the specified ID does not exist." });
+        }
+      })
+      .catch((error) => {      
+        console.log(error);
+        res.status(500).json({
+          message: "Error retrieving the action",
+        });
+      });
+});
+
+router.get('/', (req, res) => {
+    Action.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Error finding action' });
+    });
+});
+
+// router.post('/', (req, res) => {    
+//     Action.insert(req.body)
+//     .then(action => {res.status(201).json(action);})    
+//     .catch(() => {
+//       res.status(500).json({ message: 'Error adding the action to project' })
+//     })
+//   });
+
 module.exports = router;
--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -13,24 +13,72 @@ router.get('/', (req, res) => {
     .catch((err) => {
       res.status(500).json({ message: 'Error finding projects' });
     });
+});
+  
+router.post('/', (req, res) => {    
+    Project.insert(req.body)
+    .then(project => {res.status(201).json(project);})    
+    .catch(() => {
+      res.status(500).json({ message: 'Error adding project to the database' })
+    })
+});
+
+router.put('/:id', (req, res) => {
+    Project.update(req.params.id, req.body)
+      .then((project) => {
+        if (project) {
+          Project.get(req.params.id)
+            .then((project) => {
+              res.status(200).json(project);
+            })
+            .catch((err) => {
+              res
+                .status(500)
+                .json({ errorMessage: "Error reading the updated project" });
+            });
+        } else {
+          res.status(404).json({ message: "The project with the specified ID does not exist." });
+        }
+      })
+      .catch((error) => {      
+        console.log(error);
+        res.status(500).json({
+          message: "The project information could not be modified.",
+        });
+      });
+});
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    Project.remove(id)
+      .then((project) => {
+        if (project) {
+          res.status(200).json({ message: "project deleted" });
+        } else {
+          res.status(404).json({ message: "The project with the specified ID does not exist." });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: "The project could not be removed" });
+      });
   });
-  
-  
-//   router.get('/:id', (req, res) => {
-//     Project.getById(req.params.id)
-//       .then((users) => {
-//         if (users) {
-//           res.status(200).json(users);
-//         } else {
-//           res.status(404).json({ message: "The user with the specified ID does not exist." });
-//         }
-//       })
-//       .catch((error) => {      
-//         console.log(error);
-//         res.status(500).json({
-//           message: "Error retrieving the user",
-//         });
-//       });
-//   });
+
+router.get('/:id/actions', (req, res) => {    
+    Project.getProjectActions(req.params.id)
+      .then((action) => {
+        if (action) {
+          res.status(200).json(action);
+        } else {
+          res.status(404).json({ message: "Action not found" });
+        }
+      })
+      .catch((error) => {      
+        console.log(error);
+        res.status(500).json({
+          message: "Error retrieving the action",
+        });
+      });
+});
 
 module.exports = router;
